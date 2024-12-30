72ce6d57151ff8f1c2ea25907ace6e1c3c63041e
TylersHub <18tyler.rosa1@gmail.com>
2024-12-29 21:08:29 -0500

    Added new SkillBar.css file for SkillBar css code, Implemented SkillBar into About me page. (Visual representation of level of programming language skill experience)

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import HeroSection2 from "../components/HeroSection2";
 import Footer from "../components/Footer";
+import "/src/css/SkillBar.css";
 
 const About = () => {
   return (
@@ -22,22 +23,101 @@ const About = () => {
         Hname="About Me!"
         Hsubtext="I'm a College Student with an Associate's Degree in Computer Science. Now pursuing a Bachelor's Degree in Computer Science. I'm a curious and determined programmer/developer, constantly searching for challenges, finding pleasure in problem-solving, and driven by results."
       />
-      <div className="main-blue text-white container-fluid">
-        <div className="row align-items-center">
+      <div className="text-white container-fluid">
+        <div className="main-blue row align-items-center">
+          <div className="col-12 col-md ms-lg-5 ps-lg-5 mx-auto text-center">
+            <div className="skills">
+              <div className="skills-bar">
+                <div className="bar">
+                  <div className="info">
+                    <span>PYTHON</span>
+                  </div>
+                  <div className="progress-line">
+                    <span className="python"></span>
+                  </div>
+                </div>
+                <div className="bar">
+                  <div className="info">
+                    <span>JAVA</span>
+                  </div>
+                  <div className="progress-line">
+                    <span className="java"></span>
+                  </div>
+                </div>
+                <div className="bar">
+                  <div className="info">
+                    <span>JAVASCRIPT</span>
+                  </div>
+                  <div className="progress-line">
+                    <span className="javascript"></span>
+                  </div>
+                </div>
+                <div className="bar">
+                  <div className="info">
+                    <span>TYPESCRIPT</span>
+                  </div>
+                  <div className="progress-line">
+                    <span className="typescript"></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md me-lg-5 pe-lg-5 mx-auto text-center">
+            <div className="skills">
+              <div className="skills-bar">
+                <div className="bar">
+                  <div className="info">
+                    <span>C++</span>
+                  </div>
+                  <div className="progress-line">
+                    <span className="c"></span>
+                  </div>
+                </div>
+                <div className="bar">
+                  <div className="info">
+                    <span>SQL</span>
+                  </div>
+                  <div className="progress-line">
+                    <span className="sql"></span>
+                  </div>
+                </div>
+                <div className="bar">
+                  <div className="info">
+                    <span>HTML</span>
+                  </div>
+                  <div className="progress-line">
+                    <span className="html"></span>
+                  </div>
+                </div>
+                <div className="bar">
+                  <div className="info">
+                    <span>CSS</span>
+                  </div>
+                  <div className="progress-line">
+                    <span className="css"></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="main-dark row align-items-center">
           <div className="col-12 col-md-6 mx-auto text-center">
-            <h1 className="fw-bold py-4">Technical Skills</h1>
-            <p>
-              Languages: Python, Java, JavaScript, TypeScript, C++, SQL,
-              HTML/CSS
-            </p>
+            <h1 className="fw-bold py-5">Other Technical Skills</h1>
             <p>
               Libraries: React, NumPy, Beautiful Soup, SQLAlchemy, Boost,
               OpenGL, GLFW, ImGui, GLM
             </p>
+            <p>Frameworks: Flask, python, Express.js, Selenium</p>
             <p className="pb-1 pb-sm-5">
-              Frameworks: Flask, Bootstrap, Express.js, Selenium
+              Tools: VS Code, Visual Studio, Github,
             </p>
           </div>
+        </div>
+
+        <div className="main-dark row justify-content-center">
+          <div className="col-12 col-md-6 mx-auto"></div>
         </div>
       </div>
 
